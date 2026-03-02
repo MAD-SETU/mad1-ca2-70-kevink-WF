a79ad97295613155f77fd25a0390051a9bdb0982
kevink-WF <kevin.k@webfront.ie>
2026-03-02 10:03:04 +0000

    adds loggings

--- app/src/main/java/ie/gymfinder/MainActivity.kt
+++ app/src/main/java/ie/gymfinder/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import timber.log.Timber
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
+        Timber.i("Gym Acitivity started..")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
