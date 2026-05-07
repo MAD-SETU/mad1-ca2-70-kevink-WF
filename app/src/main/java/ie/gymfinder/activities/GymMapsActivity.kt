ea95bf76efce3be6384d8728388f508e750d6d98
kevink-WF <20099267@mail.wit.ie>
2026-05-07 17:45:13 +0100

    new layouts for all gyms

---
+++ app/src/main/java/ie/gymfinder/activities/GymMapsActivity.kt
@@ -0,0 +1,69 @@
+package ie.gymfinder.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import ie.gymfinder.databinding.ActivityGymMapsBinding
+import ie.gymfinder.databinding.ContentGymMapsBinding
+import ie.gymfinder.main.MainApp
+class GymMapsActivity : AppCompatActivity() {
+    lateinit var app: MainApp
+    private lateinit var binding: ActivityGymMapsBinding
+    private lateinit var contentBinding: ContentGymMapsBinding
+
+    lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityGymMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        app = application as MainApp
+
+        contentBinding = binding.include
+        contentBinding.mapView.onCreate(savedInstanceState)
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
+
+    }
+
+    private fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.gyms.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options)?.tag = it.id
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        contentBinding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        contentBinding.mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        contentBinding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        contentBinding.mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        contentBinding.mapView.onSaveInstanceState(outState)
+    }
+}
