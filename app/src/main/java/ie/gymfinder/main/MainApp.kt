fbc246d59b7e5abeaf9668794edf0be241668bc3
kevink-WF <20099267@mail.wit.ie>
2026-05-07 02:50:41 +0100

    installed firebase

--- app/src/main/java/ie/gymfinder/main/MainApp.kt
+++ app/src/main/java/ie/gymfinder/main/MainApp.kt
@@ -2,23 +2,26 @@ package ie.gymfinder.main
 
 import android.app.Application
 
-import com.google.firebase.firestore.FirebaseFirestore
 import ie.gymfinder.models.GymJsonStore
 import ie.gymfinder.models.GymStore
 import timber.log.Timber
 import timber.log.Timber.Forest.i
-
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class MainApp : Application() {
-
+    lateinit var db: DatabaseReference
     lateinit var gyms: GymStore
+    lateinit var gyms2: GymStore
+
     override fun onCreate() {
         super.onCreate()
-//       gyms = GymMemStore()
-        gyms = GymJsonStore(applicationContext)
-
 
         Timber.plant(Timber.DebugTree())
+        // gyms = GymMemStore()
+        gyms = GymJsonStore(applicationContext)
+//        gyms = GymFirebase(applicationContext)
 
         i("GymFinder started")
     }
